e948d5824468f0317e840f99ee0d1e2cc23e2201
jinges <jinges2012@gmail.com>
2014-12-15 23:20:52 +0800

    customer_controller.js

--- controlls/customer_controller.js
+++ controlls/customer_controller.js
@@ -4,43 +4,35 @@ var Customer = require('../models/customer_model');
 var unit = require('../unit/index');
 
 var customerCtr = {
-	signUpPage: function (req, res) {
-		unit.init(req, res, 'signUp', '注册');
-	},
 	signUp: function (req, res){
-		var  userName = req.body.userName
-			,passWord = req.body.userPassWord;
-
-		passWord= unit.setPassword( passWord );
+		var  userName = req.param('username')
+			,passWord = req.param('password');
 
 		var customer = {
 			userName: userName,
-			passWord: passWord
+			passWord: unit.setPassword( passWord ),
+			regDate: new Date()
 		}
-
-
+		
 		Customer.save(customer, function (err, obj){
 			if(err){
-			    req.flash('error', err);
-				return res.redirect("/");
+			    return res.stastus(500).jsonp({error: err});
 			}
-			req.session.user=obj;
-				return res.redirect("/");
+			return res.jsonp(obj);
 		});
 	},
-	signInPage: function (req, res) {
-		unit.init(req, res, 'signIn', '登录');
-	},
 	signIn: function (req, res){
-		var userName=req.body.userName;
-		var passWord= req.body.userPassWord;
-		var rememberUser = req.body.rememberUser;
+		var  userName = req.param('username')
+			,passWord = req.param('password');
 
-		passWord= unit.setPassword( passWord );
-		var query={'userName': userName, 'passWord': passWord};
+		var query={
+			'userName': userName, 
+			'passWord': unit.setPassword( passWord )
+		}
+
 		async.waterfall([
 			function (callback) {
-				Customer.findOne(query, function (err, user) {
+				Customer.find(query, function (err, user) {
 					if (err) {
 						callback(err);
 					}
@@ -53,28 +45,22 @@ var customerCtr = {
 			},
 			function (user, callback) {
 				// 2 score for login
-				Customer.upScore( user._id, 2, function (err) {
+				var id=user[0]._id;
+				Customer.modifyScore(id, 2, function (err) {
 					err ? callback(err) : callback(null, user);
 				});
 			},
 			function (user, callback) {
-				Customer.upLogDate(user._id, function (err) {
+				var id=user[0]._id;
+				Customer.upLogDate(id, function (err) {
 					err ? callback(err) : callback(null, user);
 				});
 			}
 			], function (err, result){
 				if(err) {
-				    req.flash('error', err);
-					return res.redirect("/");
+			    	return res.stastus(500).jsonp({error: err});
 				}
-
-				if( rememberUser ){
-					result.rememberUser=true;
-				}
-
-				result.score+=1;
-				req.session.user=result;
-	 			return res.redirect('/admin/index');
+				return res.jsonp(result);
 			})
 	},
 }
