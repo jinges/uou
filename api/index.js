692c5e01ee921c544b2e2f0677f622281952bf22
jinges <jinges2012@gmail.com>
2014-12-16 22:20:52 +0800

    添加了用户验证：checkMember

--- api/index.js
+++ api/index.js
@@ -10,7 +10,8 @@ var customerCtr = require('../controlls/customer_controller');
 router.get('/signUp', customerCtr.signUp);
 //登录
 router.get('/signIn', customerCtr.signIn);
-
+//验证用户是否存在
+router.get('/checkMember', customerCtr.checkMember);
 
 
 module.exports = router;
