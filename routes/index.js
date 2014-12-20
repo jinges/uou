a3b4e51e113fa4796a492c8a79989d834037f2a3
jinges <jinges2012@gmail.com>
2014-12-20 23:50:00 +0800

    修改了首页的路由方法

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var indexCtr = require('../controlls/index_controller');
-var customerCtr = require('../controlls/customer_controller');
+var merchantCtr = require('../controlls/merchant_controller');
 
 /* index */
 router.route('/')
 	.get(function (req, res){
-		res.render("index", {title:"首页"});
+		res.redirect('/signup');
 	});
-
+/*  sign up  */
+router.route('/signup')
+	.get(merchantCtr.initSignUp)
+	.post(merchantCtr.signUp);
 /*  singIn   */
 // router.route('/signIn')
 // 		.get(customerCtr.signInPage)
