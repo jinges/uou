7ab92c9bf72f38a0c789109e04e778b5bf168500
jinges <jinges2012@gmail.com>
2014-12-27 22:54:02 +0800

    添加贺卡接口

--- api/index.js
+++ api/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var customerCtr = require('../controlls/customer_controller');
+var blessingsCtr = require('../controlls/blessings_controller');
 
 
-/****** 接口 ******/
+/****** 前端接口 ******/
 //注册
 router.get('/signUp', customerCtr.signUp);
 //登录
@@ -12,5 +13,11 @@ router.get('/logIn', customerCtr.logIn);
 //验证用户是否存在
 router.get('/checkMember', customerCtr.checkMember);
 
+//创建贺卡
+router.get('/createBlessings', blessingsCtr.createBlessings);
+//阅览贺卡
+router.get('/blessings', blessingsCtr.findBlessings);
+//更新uid
+route.get('/updateUid', blessingsCtr.updateUid);
 
 module.exports = router;
