ac73220c33184aba8ff530f7a5d1273ff67ec6d9
jinges <jinges2012@gmail.com>
2015-01-03 21:35:00 +0800

    积分记录controller

---
+++ controlls/score_controller.js
@@ -0,0 +1,19 @@
+var Score = require('../models/score_model');
+var unit = require('../unit/index');
+
+var ScoreCtr = {
+	writeScore: function (uid, name, score) {
+		var ScoreObj = {
+			_id: unit.createId(),
+			Uid: uid,
+			ActionName: name,
+			score: score,
+			DateTime: new Date()
+		}
+		Score.save(ScoreObj, function (err) {
+			if(err){
+				return err;
+			}
+		});
+	}
+} 
