e50509443ee3d808423816bcab4395fbe48e2ed3
jinges <jinges2012@gmail.com>
2015-01-15 23:11:26 +0800

    修改了goods对象

--- models/goods_model.js
+++ models/goods_model.js
@@ -2,10 +2,18 @@ var mongoose=require('mongoose');
 var Schema = mongoose.Schema;
 
 var GoodsSchema = new Schema ({
-	_id: Schema.Types.ObjectId.
-	Cid: Schema.Types.ObjectId,
-	title: String,
-	lTitle: String,
-
-	
+	_id: Schema.Types.ObjectId,
+	name: String,
+	longname: String,
+	dep: Number,	//分类
+	cat: String,	//分类路径
+	totallCount: Number,   //总数量
+	lastCount: Number,    // 剩余数量
+	img: String,
+	desc: String,		//描述
+	content: String,   //详细
+	price: Number,
+	salePrice: Number,
+	mid: Schema.Types.ObjectId,
+	createDate: Date
 }); 
