b27a2ba6c13d01802a432e96016e1462bdd55d98
jinges2012@gmail.com <jinges2012@gmail.com>
2015-01-16 18:20:32 +0800

    修改了字段

--- models/category_model.js
+++ models/category_model.js
@@ -16,3 +16,32 @@ var CategorySchema = new Schema ({
 	]
 	createDate: Date 
 }); 
+
+
+var CategoryModel = mongoose.model('Category', CategorySchema);
+
+var CategoryObj = {
+	save: function (obj, callback){
+		var Category = new CategoryModel(obj);
+		Category.save(function (err, obj) {
+			callback(err, obj);
+		});
+	},
+	delete: function (query, callback) {
+		CategoryModel.remove(query, function (err) {
+			callback(err);
+		});
+	},
+	update: function (query,customer, callback) {
+		CategoryModel.update( query, customer, function (err) {
+			callback(err);
+		}); 
+	},
+	find: function (query, callback) {
+		CategoryModel.find(query, function (err, category) {
+			callback(err, category);
+		});
+	},
+}
+
+module.exports = GoodsObj;
