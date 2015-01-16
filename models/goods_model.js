d18299c000805bea5d7bc3def0caab0a8db5d03e
jinges <jinges2012@gmail.com>
2015-01-17 00:04:27 +0800

    增加属性

--- models/goods_model.js
+++ models/goods_model.js
@@ -3,18 +3,17 @@ var Schema = mongoose.Schema;
 
 var GoodsSchema = new Schema ({
 	_id: Schema.Types.ObjectId,
+	cid: Schema.Types.ObjectId,
+	mid: Schema.Types.ObjectId,
 	name: String,
 	longname: String,
-	dep: Number,	//分类
-	cat: String,	//分类路径
-	totallCount: Number,   //总数量
+	saleCount: Number,    //卖出数量
 	lastCount: Number,    // 剩余数量
 	img: String,
 	desc: String,		//描述
 	content: String,   //详细
 	price: Number,
 	salePrice: Number,
-	mid: Schema.Types.ObjectId,
 	createDate: Date
 }); 
 
